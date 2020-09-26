1ef186b8f9503d2fd452f4ef208c12c3b5f83a9b
codeclassifiers <codeclassifiers@gmail.com>
2020-09-26 20:18:01 +0530

    charts template added

---
+++ src/components/ChartList.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+function ChartsList() {
+    return (
+        <div className="container">
+        </div>
+    );
+}
+
+export default ChartsList;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="w-100 text-light">GDP Charts</div>
+        </nav>
+    );
+}
+  
+export default Header;
