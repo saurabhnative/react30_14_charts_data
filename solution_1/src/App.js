124da33d1774fcf44ab204d4bd7af9b03074d3c5
codeclassifiers <codeclassifiers@gmail.com>
2020-10-08 23:54:15 +0530

    Code refactored

---
+++ solution_1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import ChartList from './components/ChartList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <ChartList/>
+    </div>
+  );
+}
+
+export default App;
